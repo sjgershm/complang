a41c9565896d229866e1918d3c26dcf6ae4a4c86
samgershman <sam.gershman@gmail.com>
2014-01-27 22:46:52 -0500

    update

--- fmri_model.m
+++ fmri_model.m
@@ -60,10 +60,10 @@ function SPM = fmri_model(EXPT,model,submat)
         for r = 1:length(S.functional)
             niftidir = S.functional(r).niftidir;
             run = S.functional(r).run;
-            SPM.xY.P{r} = fmri_get(fullfile(niftidir,sprintf('sw*-%3.4d-*',run)));
+            SPM.xY.P{r,1} = fmri_get(fullfile(niftidir,sprintf('sw*-%3.4d-*',run));
             SPM.nscan(r) = size(SPM.xY.P{r},1);
         end
-        SPM.xY.P = vertcat(SPM.xY.P{:});
+        SPM.xY.P = char(SPM.xY.P);
         
         %loop through sessions
         for i = 1:length(S.functional)
