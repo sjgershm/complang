6adf8d2df11e0fb64a623ed14a80197a8cd767b7
samgershman <sam.gershman@gmail.com>
2014-01-29 11:36:40 -0500

    update

---
+++ complang01_get_data.m
@@ -0,0 +1,23 @@
+function data = complang01_get_data(EXPT,model,subj)
+    
+    S = EXPT.subject(subj);
+    [B names] = complang01_events(EXPT,model,subj);
+    
+    V = spm_vol(fullfile(EXPT.analysis_dir,S.name,['model',num2str(model)],'mask.img'));
+    mask = spm_read_vols(V); mask = mask~=0;
+    data.mask = mask;
+    
+    for i = 1:length(B)
+        for j = 1:length(B{i})
+            fname = sprintf('beta_%3.4d.img',B{i}(j));
+            V = spm_vol(fullfile(EXPT.analysis_dir,S.name,['model',num2str(model)],fname));
+            Y = spm_read_vols(V);
+            Y = Y(mask);
+            data.Y{i}(j,:) = Y(:);
+        end
+    end
+    
+    data.B = B;
+    data.names = names;
+    
+    save(fullfile(EXPT.analysis_dir,S.name,['model',num2str(model)],fname),'data');
