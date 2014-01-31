d1c5e3dce0c71b96db1c4ee09e8b3ca4d775aee8
samgershman <sam.gershman@gmail.com>
2014-01-30 19:35:36 -0500

    update

--- complang01_events.m
+++ complang01_events.m
@@ -12,8 +12,15 @@ function [B names] = complang01_events(EXPT,model,subj)
             names = P.names_all(3:end)';
             P = parse_para(S.functional(13).para{1},EXPT.TR);
             names = [names; P.names_all(3:end)'];
-            load(fullfile(EXPT.analysis_dir,S.name,['model',num2str(model)],'SPM'));
+            
+        case 2
+            P = parse_para(S.functional(3).para{1},EXPT.TR);
+            names = P.names_all(3:end)';
+            P = parse_para(S.functional(12).para{1},EXPT.TR);
+            names = [names; P.names_all(3:end)'];
     end
+    
+    load(fullfile(EXPT.analysis_dir,S.name,['model',num2str(model)],'SPM'));
     
     B = cell(size(names));
     for i = 1:length(names)
