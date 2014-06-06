b237a374ce2ae657b985031a9ecdb9f16ba06635
samgershman <sam.gershman@gmail.com>
2014-06-06 18:14:43 -0400

    added langloc roi function

---
+++ complang02_langloc_roi.m
@@ -0,0 +1,17 @@
+function masks = complang02_langloc_roi(EXPT,model,subj)
+    
+    % Create subject-specific langloc ROI masks.
+    %
+    % USAGE: masks = complang02_langloc_roi(EXPT,model,subj)
+    
+    load langloc_parcels
+    
+    S = EXPT.subject(subj);
+    M = ['model',num2str(model)];
+    V = spm_vol(fullfile(EXPT.analysis_dir,S.name,M,'S-N_001.img'));
+    y = spm_read_vols(V);
+    y = y>0;
+    
+    for i = 1:8
+        masks{i} = Y==i & y;
+    end
