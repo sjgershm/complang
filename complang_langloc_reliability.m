19dd62923973c828ba3acd0d195ed0b0881f15eb
samgershman <sam.gershman@gmail.com>
2014-06-06 23:10:04 -0400

    added langloc reliability

---
+++ complang_langloc_reliability.m
@@ -0,0 +1,13 @@
+function m = complang_langloc_reliability(EXPT,model,subj)
+    
+    masks = complang02_langloc_roi(EXPT,model,subj);
+    
+    names = fmri_events(EXPT,model,subj,'stim_sentencesSubsetA_2',0);
+    beta = fmri_load_beta(EXPT,model,subj,names);
+    
+    for i = 1:length(masks)
+        for k = 1:2
+            [~,p\] = compute_reliability(beta,masks{i},k);
+            m(i,k) = mean(mean(p,2)>0);
+        end
+    end
