3fddffe0cae548ed7598b889d11b6590ecfb7d08
samgershman <sam.gershman@gmail.com>
2014-06-06 23:26:15 -0400

    fixed bug

--- complang_langloc_reliability.m
+++ complang_langloc_reliability.m
@@ -9,7 +9,7 @@ function m = complang_langloc_reliability(EXPT,model,subj)
         beta = fmri_load_beta(EXPT,model,subj,names(2:end)');
         
         K = 2;
-        m = zeros(length(masks),K);
+        m{j} = zeros(length(masks),K);
         for i = 1:length(masks)
             for k = 1:K
                 [R,p] = compute_reliability(beta,masks{i},k);
