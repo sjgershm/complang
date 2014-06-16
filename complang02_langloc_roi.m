723df7d4a643d9b3a01b871ab869c6c580174367
samgershman <sam.gershman@gmail.com>
2014-06-16 18:07:06 -0400

    fixed bug

--- complang02_langloc_roi.m
+++ complang02_langloc_roi.m
@@ -12,6 +12,11 @@ function masks = complang02_langloc_roi(EXPT,model,subj)
     y = spm_read_vols(V);
     y = y>0;
     
+    % load SPM mask
+    V = spm_vol(fullfile(EXPT.analysis_dir,S.name,M,'mask.img'));
+    mask = spm_read_vols(V); mask = mask~=0;
+    
     for i = 1:8
         masks{i} = Y==i & y;
+        masks{i} = masks{i}(mask);
     end
