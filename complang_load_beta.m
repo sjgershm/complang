7b59f238e384af78085202ff282137d1c1c9a9df
samgershman <sam.gershman@gmail.com>
2014-09-05 09:27:40 -0400

    added new beta loading function

---
+++ complang_load_beta.m
@@ -0,0 +1,16 @@
+function complang_load_beta(EXPT,model,submat,F)
+    
+    for subj = submat
+        disp(num2str(subj));
+    
+        masks = complang02_langloc_roi(EXPT,model,subj);
+        names = fmri_events(EXPT,model,subj,F,0);
+        names(strcmp(names,'cue'))=[];
+        names(strcmp(names,'target'))=[];
+        beta = fmri_load_beta(EXPT,model,subj,names');
+        
+        S = EXPT.subject(subj);
+        M = ['model',num2str(model)];
+        save(fullfile(EXPT.analysis_dir,S.name,M,['beta_',F]),'beta','masks');
+        
+    end
