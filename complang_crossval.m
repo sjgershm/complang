abfbe378ccde31ada6585bbba5e5f68330c84aad
samgershman <sam.gershman@gmail.com>
2014-09-07 22:25:56 -0400

    fixed cross-validation

--- complang_crossval.m
+++ complang_crossval.m
@@ -1,26 +1,29 @@
-function [rank,C] = complang_crossval(beta,masks)
+function rank = complang_crossval(beta,masks)
     
-    rank = zeros(length(beta),length(masks));
+    rank = nan(length(beta),length(masks),2);
     for i = 1:length(masks)
         disp(num2str(i));
         
-        % collect all data
-        y = []; x = [];
         V = sum(masks{i});
         J = length(beta);
         y1 = zeros(J,V); y2 = zeros(J,V);
+        include = true(J,1);
         for j = 1:J
-            q = round(size(beta{j},1)/2);
-            y1(j,:) = mean(beta{j}(1:2:end,masks{i}));
-            y2(j,:) = mean(beta{j}(2:2:end,masks{i}));
-            %y1(j,:) = mean(beta{j}(4:10,masks{i}));
-            %y2(j,:) = mean(beta{j}(11:16,masks{i}));
+            if size(beta{j},1)<2; include(j)=0; end
+            y1(j,:) = nanmean(beta{j}(1:2:end,masks{i}));
+            y2(j,:) = nanmean(beta{j}(2:2:end,masks{i}));
         end
         
-        r = pdist2(y1,y2,'correlation');
-        for j=1:J
+        include = find(include);
+        y1 = y1(include,:); y2 = y2(include,:); J = size(y1,1);
+        
+        r = pdist2(y1,y2,'euclidean');
+        for j = 1:J
             [~,k]=sort(r(j,:),'descend');
-            rank(j,i)=(find(k==j))/J;
+            rank(include(j),i,1)=(find(k==j))/J;
+            [~,k]=sort(r(:,j),'descend');
+            rank(include(j),i,2)=(find(k==j))/J;
         end
-        
     end
+    
+    rank = squeeze(nanmean(rank,3));
