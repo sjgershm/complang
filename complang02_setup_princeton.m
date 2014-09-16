ae94c87515e350a342186f4e0526449e77df4379
Francisco Pereira <tsuipen>
2014-09-16 15:01:43 -0400

    finished adding all the sessions for PU subject 1

--- complang02_setup_princeton.m
+++ complang02_setup_princeton.m
@@ -1,4 +1,4 @@
-function EXP = complang02_setup_princeton()
+function EXPT = complang02_setup_princeton()
   
 % Collect information about the experiment into EXPT structure.
 %
@@ -11,7 +11,7 @@ function EXP = complang02_setup_princeton()
   EXPT.data_dir     = '/Volumes/Store/Datasets/MITPU/data02';
     
   %addpath(genpath('/mindhive/nklab/projects/compositionality_lang/scripts/spm8'));
-  addpath(genpath('/usr/local/src//spm8/'));
+  addpath(genpath('/usr/local/src/spm8/'));
   addpath('/usr/local/src/MIT/complang');
   addpath('/usr/local/src/MIT/fmri');
   
@@ -20,25 +20,228 @@ function EXP = complang02_setup_princeton()
   %----------------------------------%
   EXPT.subject(1).name = 'sub01';
   
+  % sentences
   dicomdir1 = fullfile(EXPT.data_dir,'SUB01_20140904','dicoms');
   niftidir1 = fullfile(EXPT.data_dir,'SUB01_20140904','nii');
   paradir1  = fullfile(EXPT.data_dir,'SUB01_20140904','onsets');
+  % simwords + language localizer
   dicomdir2 = fullfile(EXPT.data_dir,'SUB01_20140910','dicoms');
   niftidir2 = fullfile(EXPT.data_dir,'SUB01_20140910','nii');
   paradir2  = fullfile(EXPT.data_dir,'SUB01_20140910','onsets');
+  % sentences
   dicomdir3 = fullfile(EXPT.data_dir,'SUB01_20140911','dicoms');
   niftidir3 = fullfile(EXPT.data_dir,'SUB01_20140911','nii');
   paradir3  = fullfile(EXPT.data_dir,'SUB01_20140911','onsets');
 
-  EXPT.subject(1).anatomical.run = TODO;
-  EXPT.subject(1).anatomical.dicomdir = dicomdir1;
-  EXPT.subject(1).anatomical.niftidir = niftidir1;
+  EXPT.subject(1).anatomical.run = 2;
+  EXPT.subject(1).anatomical.dicomdir = dicomdir2;
+  EXPT.subject(1).anatomical.niftidir = niftidir2;
   
-  EXPT.subject(1).functional(1).run = 5;
-  EXPT.subject(1).functional(1).name = 'stim_words1';
-  EXPT.subject(1).functional(1).dicomdir = dicomdir2;
-  EXPT.subject(1).functional(1).niftidir = niftidir2;
-  EXPT.subject(1).functional(1).para{1} = fullfile(paradir1,'subj01b_05_ICGT.para');
+  % alternative, if we can have one structural per run
+  %  aidx = 1;
+  %EXPT.subject(1).anatomical(aidx).run = 2;
+  %  EXPT.subject(1).anatomical(aidx).dicomdir = dicomdir1;
+  %  EXPT.subject(1).anatomical(aidx).niftidir = niftidir1;
+  %  aidx = aidx + 1;
+  %  EXPT.subject(1).anatomical(aidx).run = 2;
+  %  EXPT.subject(1).anatomical(aidx).dicomdir = dicomdir2;
+  %  EXPT.subject(1).anatomical(aidx).niftidir = niftidir2;
+  %  aidx = aidx + 1;
+  %  EXPT.subject(1).anatomical(aidx).run = 2;
+  %  EXPT.subject(1).anatomical(aidx).dicomdir = dicomdir3;
+  % EXPT.subject(1).anatomical(aidx).niftidir = niftidir3;
+  
+  fidx = 1;
+
+  %% session 1
+  
+  EXPT.subject(1).functional(fidx).run = 4;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesAllSubset_1';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir1;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir1;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir1,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 6;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesAllSubset_2';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir1;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir1;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir1,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 8;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesAllSubset_3';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir1;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir1;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir1,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 10;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesAllSubset_4';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir1;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir1;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir1,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 12;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesAllSubset_5';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir1;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir1;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir1,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 14;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesAllSubset_6';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir1;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir1;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir1,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 16;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesAllSubset_7';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir1;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir1;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir1,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 18;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesAllSubset_8';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir1;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir1;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir1,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 20;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesSubsetA_1';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir1;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir1;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir1,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 22;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesSubsetB_1';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir1;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir1;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir1,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  
+  
+  %% session 2
+
+  EXPT.subject(1).functional(fidx).run = 4;
+  EXPT.subject(1).functional(fidx).name = 'stim_words1';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir2;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir2;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir2,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 6;
+  EXPT.subject(1).functional(fidx).name = 'stim_words2';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir2;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir2;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir2,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 8;
+  EXPT.subject(1).functional(fidx).name = 'stim_words3';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir2;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir2;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir2,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 10;
+  EXPT.subject(1).functional(fidx).name = 'stim_words4';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir2;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir2;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir2,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 12;
+  EXPT.subject(1).functional(fidx).name = 'stim_words5';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir2;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir2;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir2,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 14;
+  EXPT.subject(1).functional(fidx).name = 'stim_words6';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir2;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir2;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir2,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 16;
+  EXPT.subject(1).functional(fidx).name = 'stim_words7';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir2;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir2;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir2,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 18;
+  EXPT.subject(1).functional(fidx).name = 'stim_words8';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir2;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir2;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir2,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 20;
+  EXPT.subject(1).functional(fidx).name = 'LangLoc_1';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir2;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir2;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir2,sprintf('data_%s.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 22;
+  EXPT.subject(1).functional(fidx).name = 'LangLoc_2';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir2;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir2;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir2,sprintf('data_%s.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  
+  %% session 3
+  
+  EXPT.subject(1).functional(fidx).run = 4;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesAllSubset_1';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir3;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir3;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir3,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 6;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesAllSubset_2';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir3;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir3;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir3,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 8;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesAllSubset_3';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir3;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir3;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir3,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 10;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesAllSubset_4';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir3;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir3;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir3,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 12;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesAllSubset_5';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir3;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir3;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir3,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 14;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesAllSubset_6';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir3;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir3;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir3,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 16;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesAllSubset_7';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir3;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir3;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir3,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 18;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesAllSubset_8';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir3;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir3;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir3,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 20;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesSubsetA_1';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir3;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir3;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir3,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
+  EXPT.subject(1).functional(fidx).run = 22;
+  EXPT.subject(1).functional(fidx).name = 'stim_sentencesSubsetB_1';
+  EXPT.subject(1).functional(fidx).dicomdir = dicomdir3;
+  EXPT.subject(1).functional(fidx).niftidir = niftidir3;
+  EXPT.subject(1).functional(fidx).para{1} = fullfile(paradir3,sprintf('data_%s_ICGT_zero.para',EXPT.subject(1).functional(fidx).name));
+  fidx = fidx + 1;
   
   %----------------------------------%
   %           SUBJECT 2              
