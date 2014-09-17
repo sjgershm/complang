8577ca3bdec6a663c7b6b6a44ac3433c635bcfde
samgershman <sam.gershman@gmail.com>
2014-09-17 19:30:05 -0400

    bug fix

--- complang02_langloc_roi.m
+++ complang02_langloc_roi.m
@@ -16,14 +16,13 @@ function masks = complang02_langloc_roi(EXPT,model,subj,group)
     % load SPM mask
     V = spm_vol(fullfile(EXPT.analysis_dir,S.name,M,'run1','mask.img'));
     mask = spm_read_vols(V); mask = mask~=0;
+    Y = Y(mask);
     
     for i = 1:8
-        masks{i} = Y==i & y;
-        masks{i} = masks{i}(mask);
+        masks{i} = Y==i & y';
         
         % use the group parcel if no voxels survive
         if ~any(masks{i}) || group
             masks{i} = Y==i;
-            masks{i} = masks{i}(mask);
         end
     end
