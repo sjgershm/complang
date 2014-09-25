47d8ada0f0096ae7c420709180a1ed7daa5d22a2
Walid <theinterwebnets@gmail.com>
2014-09-25 19:01:22 -0400

    made get reversibles betas

--- complang02_get_betas_reversibles.m
+++ complang02_get_betas_reversibles.m
@@ -6,13 +6,15 @@
 % 2014-09-24 Initial function (Walid)
 
 function complang02_get_betas_reversibles(EXPT,model,subj)
-    names={'S','N'};
+    n=strfind(EXPT.analysis_dir, '/');
+    filename = fullfile(EXPT.analysis_dir(1:n(end)),'complang','reversibles.mat');
+    load(filename);
+    names=sentences';
     [beta, mask] = fmri_load_beta(EXPT,model,subj,names);
-    [~,p] = ttest2(beta{1},beta{2}); 
 	cdir=cd;
+    names=names';
 	if ~exist(fullfile(EXPT.analysis_dir,EXPT.subject(subj).name,'betas'),'dir'); mkdir(fullfile(EXPT.analysis_dir,EXPT.subject(subj).name,'betas')); end
 	cd(fullfile(EXPT.analysis_dir,EXPT.subject(subj).name,'betas'));
-    save('reversibles','p','beta');
+    save('reversiblesBetas','beta','mask','names');
 	cd(cdir);
-   
 end
